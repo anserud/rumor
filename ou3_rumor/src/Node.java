f62dfc66390679fc8974d0694966a9695aacd89d
anserud <alexanderanserud@live.se>
2013-05-20 12:44:53 +0200

    Lade till metoder och attributer

--- ou3_rumor/src/Node.java
+++ ou3_rumor/src/Node.java
@@ -1,4 +1,30 @@
+import java.util.ArrayList;
+
 public class Node
 {
-    
+   private long event_P;
+   private long agent_P;
+   private ArrayList<Node> neighbourList;
+   private ArrayList<Event> eventList;
+   
+   public Node(){
+	   
+   }
+   
+   public void addNeighbour(Node n, long Distance)
+   {
+	   
+   }
+   
+   public void ProcessMessage(Message message){
+	   
+   }
+   
+   private void tryCreateAgent(){
+	   
+   }
+   private void tryCreateEvent()
+   {
+	   
+   }
 }
