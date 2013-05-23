5e1dd2011f730a38e1e2950a09566788e0d0b7a6
Anserud <alexanderanserud@live.se>
2013-05-23 12:20:41 +0200

    Skapade route klassens grundläggande funktionalitet

--- ou3_rumor/src/Route.java
+++ ou3_rumor/src/Route.java
@@ -1,4 +1,57 @@
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Route.
+ */
 public class Route
 {
+	
+	/** The event id. */
+	private int eventID;
     
+    /** The direction. */
+    private Node direction;
+    
+    /** The distance. */
+    private int distance;
+    
+    /**
+     * Instantiates a new route.
+     *
+     * @param eventID the event id
+     * @param direction the direction
+     * @param distance the distance
+     */
+    public Route(int eventID, Node direction, int distance)
+    {
+    	this.eventID = eventID;
+    	this.direction = direction;
+    	this.distance = distance;
+    }
+    
+    /**
+     * Gets the event id.
+     *
+     * @return the event id
+     */
+    public int getEventID() {
+		return eventID;
+	}
+
+	/**
+	 * Gets the direction.
+	 *
+	 * @return the direction
+	 */
+	public Node getDirection() {
+		return direction;
+	}
+
+	/**
+	 * Gets the distance.
+	 *
+	 * @return the distance
+	 */
+	public int getDistance() {
+		return distance;
+	}
 }
