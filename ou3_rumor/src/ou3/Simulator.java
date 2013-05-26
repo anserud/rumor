eed2ec6e54d53bd28e6c27deced9995b6bbfeb06
rednatto <rednatto@gmail.com>
2013-05-26 13:39:04 +0200

    commit

--- ou3_rumor/src/ou3/Simulator.java
+++ ou3_rumor/src/ou3/Simulator.java
@@ -8,19 +8,9 @@ public class Simulator
     private int     numberOfSteps;
     private Network net;
     
-    public Simulator( int gridSize, int numberOfSteps, int eventProbability,
-            int agentProbability, int queryNodes, int queryTime, int agentTTL,
-            int queryTTL)
-    {
-        Node.setAgentProbability( agentProbability );
-        Node.setEventProbability( eventProbability );
-        
-        AgentMessage.setLifeLength( agentTTL );
-        QueryMessage.setLifeLength( queryTTL );
-        QueryMessage.setExpected( queryTTL * 8 );
-        
-        this.numberOfSteps = numberOfSteps;
-        this.net = new Network( gridSize, queryNodes, queryTime );
+    public Simulator( )
+    {   
+        this.net = new Network( "H:\\git\\ou3\\ou3_rumor\\src\\configuration.xml" );
     }
     
     public void runSimulator()
